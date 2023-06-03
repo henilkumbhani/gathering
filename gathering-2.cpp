31f8a2d66576cf605004692f620ea7c68964104c
henilkumbhani <131843236+henilkumbhani@users.noreply.github.com>
2023-06-03 10:24:05 +0530

    Add files via upload

---
+++ gathering-2.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+sum(int arr[],int n)
+{
+	 int i,sum=0;
+	 for(i=0;i<n;i++)
+	 {
+	 	sum= sum+arr[i];
+	 }
+	 return sum;
+}
+main()
+{
+       int i,n,ans;
+       printf("enter elements =");
+       scanf("%d",&n);
+       printf("\n");
+       int arr[n];
+       for(i=0;i<n;i++)
+       {
+               	printf("enter number =");
+              	scanf("%d",&arr[i]);
+
+	   }
+	   ans=sum(arr,n);
+	   printf("\nsum=%d",ans);
+}
